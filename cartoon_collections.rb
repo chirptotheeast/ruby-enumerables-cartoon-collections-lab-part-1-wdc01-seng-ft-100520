924929df4560e908ffbd436915d8a0716282e2f4
Alyssa Easterly <alyssa.easterly@yahoo.com>
2020-09-22 19:12:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
-  greet_characters
+  greet_characters |element|
 end
 
 def list_dwarves(array)
