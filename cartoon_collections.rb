c474d8c1d057e7729526b6913b8381ffbe649918
Alyssa Easterly <alyssa.easterly@yahoo.com>
2020-09-22 19:14:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
-  greet_characters.each do |element|
-     puts " Hello #{element}!"
+  greet_characters.each do |characters_array|
+     puts " Hello #{characters_array}!"
    end
   
 end
